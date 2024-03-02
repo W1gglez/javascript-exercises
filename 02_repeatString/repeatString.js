8c2c9b94bd85e627008e12e2f149af09db045a7d
lweigel <snr.wiggels@gmail.com>
2024-03-01 18:43:23 -0600

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(toBeRepeated, timesToRepeat) {
+    if(timesToRepeat < 0) return 'ERROR';
+    let output = '';
+    for(timesToRepeat; timesToRepeat>=1; timesToRepeat--){
+        output += toBeRepeated;
+    }
+    return(output)
 };
 
 // Do not edit below this line
