e7ad285c0453e3c9c10de5482bfb2275ee35eee6
lweigel <snr.wiggels@gmail.com>
2024-03-01 18:53:10 -0600

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let output = "";
+    for (let i = 1; (string.length - i) >= 0; i++){
+        output += string.charAt(string.length - i);
+    }
+    return(output);
 };
 
 // Do not edit below this line
