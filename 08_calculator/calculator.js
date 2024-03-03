7320f8924cffe0ca33f9bd280f6e91d28c793f17
lweigel <snr.wiggels@gmail.com>
2024-03-02 19:27:00 -0600

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function (int1, int2) {
+  return int1 + int2;
 };
 
-const subtract = function() {
-	
+const subtract = function (int1, int2) {
+  return int1 - int2;
 };
 
-const sum = function() {
-	
+
+const sum = function (args) {
+  let sum = 0;
+  for (const arg of args) {
+    sum += arg;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (args) {
+  let product = 1;
+  for (const arg of args) {
+    product *= arg;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function (int1, int2) {
+  let product = 1;
+  for (let i = int2; i > 0; i--) {
+    product *= int1;
+  }
+  return product;
 };
 
-const factorial = function() {
-	
+const factorial = function (int) {
+  let product = 1;
+  for (let i = int; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
@@ -29,5 +46,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
