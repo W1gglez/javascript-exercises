5ef1cb82b26b2abf00c5353526323c190b1b459f
lweigel <snr.wiggels@gmail.com>
2024-03-02 19:50:09 -0600

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (isPalindrome) {
+    const processedString = isPalindrome.toLowerCase().replace(/[^a-z0-9]/g, '')
+    return processedString.split("").reverse().join("") == processedString;
 };
+
+palindromes('race car') 
 
 // Do not edit below this line
 module.exports = palindromes;
