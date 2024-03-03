6709b9be3d03e0c8d8346caa1570d098f481e016
lweigel <snr.wiggels@gmail.com>
2024-03-02 20:09:53 -0600

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+    member = Number(member);
+    let int1 =0;
+    let int2 = 1;
+    if(member < 0){
+        return 'OOPS'
+    }
+    for (let i =1; i <= member; i++){
+        let int3 = int1;
+        int1 = int2 + int1;
+        int2 = int3;
+    }
+    return int1;
 };
+
+fibonacci(4);
 
 // Do not edit below this line
 module.exports = fibonacci;
